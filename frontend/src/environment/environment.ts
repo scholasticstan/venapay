55721461c63683dd5b0d63ebd415ad55c6adb9c4
scholasticstan <alu.uzorka.stanley@gmail.com>
2025-03-05 20:03:28 -0800

    added firestore

---
+++ frontend/src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+firebase: {
+  apiKey: "AIzaSyAUeA6cPENEmXEmywCHFhkpUVYUZatHt2w",
+  authDomain: "venapay.firebaseapp.com",
+  projectId: "venapay",
+  storageBucket: "venapay.firebasestorage.app",
+  messagingSenderId: "111639722446",
+  appId: "1:111639722446:web:29447a0f0439afd045139f"
+}
+};
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { provideStore } from '@ngrx/store';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideStore(), provideFirebaseApp(() => initializeApp({ projectId: "venapay", appId: "1:111639722446:web:29447a0f0439afd045139f", storageBucket: "venapay.firebasestorage.app", apiKey: "AIzaSyAUeA6cPENEmXEmywCHFhkpUVYUZatHt2w", authDomain: "venapay.firebaseapp.com", messagingSenderId: "111639722446" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+]
 });
